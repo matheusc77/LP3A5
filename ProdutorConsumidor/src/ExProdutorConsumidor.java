48a30bcb6af0643c4d3783cd1d9dbea6caab2aba
Matheus Casagrande <matheuscasagrand@gmail.com>
2022-08-29 18:43:12 -0300

    Produtor Consumirdor

---
+++ ProdutorConsumidor/src/ExProdutorConsumidor.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ExProdutorConsumidor {
+    private final int MAX = 10;
+    private ArrayList<Integer> buffer = new ArrayList<>(MAX);
+    private Object cheio = new Object();
+    private Object vazio = new Object();
+
+    public static void main(String[] args) {
+        new ExProdutorConsumidor().executaParaelo();
+
+    }
+
+    public void executaParaelo(){
+        Produtor p = new Produtor();
+        Consumidor c = new Consumidor();
+        new Thread(p).start();
+        new Thread(c).start();
+
+    }
+
+    public class Produtor implements Runnable{
+
+        Random rnd = new Random();
+        @Override
+        public void run() {
+
+            while (true){
+                synchronized (cheio){
+                    if(buffer.size() == MAX ){
+                        try {
+                            cheio.wait();
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                            return;
+                        }
+                    }
+                }
+
+                int num = rnd.nextInt(100);
+                synchronized (buffer){
+                    buffer.add(num);
+                }
+
+                synchronized (vazio){
+                    if(buffer.size()==1){
+                        vazio.notify();
+                    }
+                }
+            }
+        }
+    }
+
+
+    public class Consumidor implements Runnable{
+        @Override
+        public void run() {
+
+            while (true){
+                synchronized (vazio){
+                    if(buffer.size() == 0){
+                        try {
+                            vazio.wait();
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                            return;
+                        }
+                    }
+                }
+
+
+                synchronized (buffer){
+                    System.out.println(buffer.remove(0));
+                }
+
+
+                synchronized (cheio){
+                    if(buffer.size()==0){
+                        cheio.notify();
+                    }
+                }
+            }
+        }
+    }
+}
